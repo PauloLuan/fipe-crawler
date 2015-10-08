7f4f7c885797c83dfaa2ae5666c880a593265c71
Paulo Luan <pauloluan.inova@gmail.com>
2015-10-08 11:29:31 -0300

    asserting object is not null

--- test/index.js
+++ test/index.js
@@ -7,7 +7,7 @@ describe('fipe-crawler', function() {
   it('should do a request and return the car brands', function(done) {
     var brands = fipeCrawler.getCarBrands()
       .then(function(brands) {
-        assert(brands.length > 0, 'expect car brands not to be null');
+        assert(brands !== null, 'expect car brands not to be null');
         assert(brands.length > 0, 'expect car brands to has more than one brand');
 
         done();
