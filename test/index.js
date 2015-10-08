0a89b20e25abca568cf3a7af3b6e6a95218810dd
Paulo Luan <pauloluan.inova@gmail.com>
2015-10-08 10:12:04 -0300

    upgraded gulp-istanbul version

--- test/index.js
+++ test/index.js
@@ -5,11 +5,12 @@ var assert = require('assert'),
 
 describe('fipe-crawler', function() {
   it('should do a request and return the car brands', function(done) {
-    var brands = fipeCrawler.getCarBrands().then(function(brands) {
-      assert(brands.length > 0, 'expect car brands not to be null');
-      assert(brands.length > 0, 'expect car brands to has more than one brand');
+    var brands = fipeCrawler.getCarBrands()
+      .then(function(brands) {
+        assert(brands.length > 0, 'expect car brands not to be null');
+        assert(brands.length > 0, 'expect car brands to has more than one brand');
 
-      done();
-    });
+        done();
+      });
   });
 });
