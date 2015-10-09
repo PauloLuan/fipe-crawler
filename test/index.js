8195914a62130e5aaf710b6dad67254e4571200c
Paulo Luan <pauloluan.inova@gmail.com>
2015-10-09 11:49:08 -0300

    added first validated mock version

--- test/index.js
+++ test/index.js
@@ -6,55 +6,42 @@ var assert = require('assert'),
 describe('fipe-crawler', function() {
   this.timeout(6000);
 
-  var brands, models;
-
   it('should do a request and return the car brands', function() {
-    brands = fipeCrawler.getCarBrands();
+    var brands = fipeCrawler.getCarBrands();
 
     assert(brands !== null, 'expect car brands not to be null');
     assert(brands.length > 0, 'expect car brands to has more than one brand');
   });
 
-  xit('should download car models using the returned brands', function(done) {
-    fipeCrawler.getCarModels(brands)
-      .then(function(receivedModels) {
-        models = JSON.parse(receivedModels);
+  it('should download car models using the returned brands', function() {
+    var mockBrands = require('./mocks/1-IndicesConsulta-ConsultarMarcas.json');
+    var models = fipeCrawler.getCarModels(mockBrands[0]);
 
-        assert(models !== null, 'expect car models not to be null');
-        assert(models.Modelos.length > 0, 'expect car models to has more than one');
-
-        done();
-      });
+    assert(models !== null, 'expect car models should not to be null');
+    assert(models.Modelos.length > 0, 'expect car models to has more than one');
   });
 
-  xit('should download car models using years', function(done) {
-    fipeCrawler.getCarYear()
-      .then(function(years) {
-        assert(years !== null, 'expect car years not to be null');
-        assert(years.length > 0, 'expect car years to has more than one brand');
+  it('should download car models using years', function() {
+    var years = fipeCrawler.getCarYear();
 
-        done();
-      });
+    assert(years !== null, 'expect car years not to be null');
+    assert(years.length > 0, 'expect car years to has more than one brand');
   });
 
-  xit('should do what...', function(done) {
-    fipeCrawler.getCarModelByYear()
-      .then(function(models) {
-        assert(models !== null, 'expect car models not to be null');
-        assert(models.length > 0, 'expect car models to has more than one brand');
+  it('should do what...', function() {
+    var models = fipeCrawler.getCarModelByYear();
 
-        done();
-      });
+    assert(models !== null, 'expect car models not to be null');
+    assert(models.length > 0, 'expect car models to has more than one brand');
   });
 
-  xit('should do what...', function(done) {
-    fipeCrawler.getCarWxithAllParams()
-      .then(function(carModel) {
-        assert(carModel !== null, 'expect car carModel not to be null');
-        assert(carModel.length > 0, 'expect car carModel to has more than one brand');
+  it('download the complete information about a car model', function() {
+    var carModel = fipeCrawler.getCarWithAllParams();
 
-        done();
-      });
+    assert(carModel !== null, 'expect car carModel not to be null');
+    assert(carModel.Valor !== null, 'expect car carModel has property Valor');
+    assert(carModel.Marca !== null, 'expect car carModel has property Marca');
+    assert(carModel.Modelo !== null, 'expect car carModel has property Marca');
   });
 
 });
